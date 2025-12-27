3a1ca7ce3ca271a421ec9cccb9a0809aa6363de3
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-12-28 01:21:26 +0530

    Add files via upload

---
+++ Showroom.java
@@ -0,0 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Showroom implements utility {
+    String showroom_name ;
+    String showroom_add ;
+    String manager_name;
+    int total_employees;
+    int total_cars_in_stock = 0;
+
+    @Override
+    public void get_details(){
+        System.out.println("Showroom Name: " + showroom_name);
+        System.out.println("Showroom Address: " + showroom_add);
+        System.out.println("Manager Name: "+ manager_name);
+        System.out.println("Total No. of Employee: " + total_employees);
+        System.out.println("Total cars in Stock: "+ total_cars_in_stock);
+    }
+
+    @Override
+    public void set_details (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("============================== *** Enter Showroom Details *** ==============================");
+        System.out.println();
+        System.out.print("Showroom Name: ");
+        showroom_name = sc.nextLine();
+        System.out.print("Showroom Address: ");
+        showroom_add = sc.nextLine();
+        System.out.print("Manager Name: ");
+        manager_name = sc.nextLine();
+        System.out.print("Total No. of Employee: ");
+        total_employees = sc.nextInt();
+        System.out.print("Total cars in Stock: ");
+        total_cars_in_stock = sc.nextInt();
+    }
+
+}
